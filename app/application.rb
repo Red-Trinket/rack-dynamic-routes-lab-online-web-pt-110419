dff0aaa1e98d98316a4f9144ee313410bb6980bc
Matthew Jones <matts3371@gmail.com>
2020-02-11 01:17:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,25 @@
-class Application 
-  
+class Application
+
+  @@items = []
+
   def call(env)
-    resp = Rack::Response.new 
     req = Rack::Request.new(env)
-    
-    if req.path.match(/items/)
-      item_name = req.path.split("/items").last 
-      item = Item.find{|i| i.name == item_name}
-      
-      resp.write item.price 
-    else 
-      resp.write "404"
-    end 
-  end 
-end 
+    resp = Rack::Response.new
+
+    if req.path.match(/items\/.+/)
+      item_name = req.path.split("/items/").last
+      item = @@items.detect { |i| i.name == item_name }
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
